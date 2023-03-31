e481bd830444cdb75af39cf93c7f823e9982546e
nofrari <nora.riglthaler@gmail.com>
2023-03-31 12:34:38 +0200

    add scanner page

--- frontend/lib/pages/scanner.dart
+++ frontend/lib/pages/scanner.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:dio/dio.dart';
 
 class Scanner extends StatefulWidget {
   const Scanner({super.key});
@@ -8,12 +11,67 @@ class Scanner extends StatefulWidget {
 }
 
 class _ScannerState extends State<Scanner> {
+  final ImagePicker _picker = ImagePicker();
+  File? imageFile;
+
+  Dio dio = Dio();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Scanner Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            imageFile == null
+                ? const Icon(Icons.add_photo_alternate)
+                : Column(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        height: 200,
+                        child: Image.file(imageFile!, fit: BoxFit.cover),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {
+                          _uploadImage();
+                        },
+                        child: const Text("UPLOAD"),
+                      )
+                    ],
+                  ),
+            FloatingActionButton(
+              onPressed: () {
+                _getFromGallery();
+              },
+              child: const Text("Pick Image from Gallery",
+                  style: TextStyle(
+                      color: Colors.white70, fontWeight: FontWeight.bold)),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  _getFromGallery() async {
+    XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  _uploadImage() async {
+    var formData = FormData.fromMap(
+      {
+        "image": await MultipartFile.fromFile(imageFile!.path),
+      },
+    );
+    var response =
+        await dio.post("http://10.0.2.2:3000/upload", data: formData);
+    debugPrint(response.toString());
   }
 }
