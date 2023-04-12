4871cc96d8d44909a3edfa8662cac4dd31547ab3
nofrari <nora.riglthaler@gmail.com>
2023-04-12 16:31:17 +0200

    add scanner to backend
    With needed packages: mindee, multer, path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,52 @@
 import express from 'express';
+import multer from 'multer';
+const mindee = require("mindee");
+import path from 'path';
+
 import usersRouter from './routes/users';
 
 const app = express();
+const mindeeClient = new mindee.Client({ apiKey: "408ec2ab38cadbb87b1d5b976e3644b9" });
+
 app.use(express.json());
+app.use(usersRouter);
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
 app.post('/', (req, res) => {
     res.status(201).send('Hello world');
 });
 
-app.use(usersRouter);
+app.post("/upload", upload.single("image"), (req, res) => {
+    if (req.file === undefined || req.file === null) {
+        return res.status(422).send("Image cannot be empty");
+    }
+    let file = req.file;
 
-app.listen(5432);
+    //mindee parser
+    const apiResponse = mindeeClient
+        .docFromPath("./src/uploads/" + file?.filename)
+        .parse(mindee.InvoiceV4);
+
+
+    apiResponse.then((resp: { document: any; }) => {
+        console.log(resp.document);
+    });
+
+    return res.status(200).send({ name: file?.filename });
+});
+
+app.listen(5432, () => {
+    console.log("Listening on Port 3000!");
+});
 
 //just for testing
 export function add(first: number, second: number) {
