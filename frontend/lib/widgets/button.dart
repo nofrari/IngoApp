194c996b448524bcb6bb7378ab8807c2e00c73e7
itsjustmoniii <suessmonika8@gmail.com>
2023-04-13 16:19:30 +0200

    add button widget

---
+++ frontend/lib/widgets/button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+import '../constants/fonts.dart';
+import '../constants/values.dart';
+
+class Button extends StatelessWidget {
+  const Button({required this.btnText, required this.onTap, super.key});
+
+  final String btnText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          foregroundColor: AppColor.neutral100,
+          backgroundColor: AppColor.neutral500,
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(Values.buttonRadius),
+              side: BorderSide(color: AppColor.callToActionLight, width: 2)),
+        ),
+        child: Text(btnText, style: Fonts.button, textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
