dd0345bdfc8f584490549de3425363f801fe70d4
nofrari <nora.riglthaler@gmail.com>
2023-04-20 16:28:38 +0200

    add pdfPreview and dropdown_test

--- frontend/lib/pages/manual-entry.dart
+++ frontend/lib/pages/manual-entry.dart
@@ -5,6 +5,8 @@ import 'package:frontend/constants/fonts.dart';
 import 'package:frontend/constants/values.dart';
 import 'package:frontend/widgets/input_fields/input_field.dart';
 import 'package:frontend/widgets/input_fields/datepicker_field.dart';
+import 'package:frontend/widgets/input_fields/dropdown_field_test.dart';
+import 'package:frontend/widgets/pdf_preview.dart';
 
 class ManualEntry extends StatefulWidget {
   const ManualEntry({super.key});
@@ -19,6 +21,8 @@ class _ManualEntryState extends State<ManualEntry> {
       FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z0-9#+:()&/^\-{2}|\s]"));
   TextInputType numeric = TextInputType.number;
   TextInputType text = TextInputType.text;
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -45,25 +49,41 @@ class _ManualEntryState extends State<ManualEntry> {
               color: AppColor.neutral500),
           margin: const EdgeInsets.all(20),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                InputField(
-                  lblText: "Betrag",
-                  formatter: digits,
-                  keyboardType: numeric,
-                ),
-                InputField(
-                  lblText: "Unternehmen",
-                  formatter: letters,
-                  keyboardType: text,
-                ),
-                InputField(
-                  lblText: "Beschreibung",
-                  formatter: letters,
-                  keyboardType: text,
-                ),
-                const DatepickerField(),
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  InputField(
+                    lblText: "Betrag",
+                    formatter: digits,
+                    keyboardType: numeric,
+                  ),
+                  InputField(
+                    lblText: "Unternehmen",
+                    formatter: letters,
+                    keyboardType: text,
+                  ),
+                  InputField(
+                    lblText: "Beschreibung",
+                    formatter: letters,
+                    keyboardType: text,
+                  ),
+                  const DatepickerField(),
+                  const PdfPreview(pdfUrl: '/assets/pdf/sample.pdf'),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
