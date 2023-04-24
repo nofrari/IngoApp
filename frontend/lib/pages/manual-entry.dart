8654670840560cf9152bca1fbf7547750893a231
nofrari <nora.riglthaler@gmail.com>
2023-04-24 20:55:51 +0200

    update manual entry screen
    Added controllers for each field, added gesturedetector for closing keyboard on tap outside, added function to send data to backend

--- frontend/lib/pages/manual-entry.dart
+++ frontend/lib/pages/manual-entry.dart
@@ -6,6 +6,7 @@ import 'package:frontend/constants/values.dart';
 import 'package:frontend/widgets/input_fields/input_field.dart';
 import 'package:frontend/widgets/input_fields/datepicker_field.dart';
 import 'package:frontend/widgets/pdf_preview.dart';
+import 'package:dio/dio.dart';
 
 class ManualEntry extends StatefulWidget {
   const ManualEntry({super.key});
@@ -18,75 +19,120 @@ class _ManualEntryState extends State<ManualEntry> {
   TextInputFormatter digits = FilteringTextInputFormatter.digitsOnly;
   TextInputFormatter letters =
       FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z0-9#+:()&/^\-{2}|\s]"));
+
   TextInputType numeric = TextInputType.number;
   TextInputType text = TextInputType.text;
 
+  TextEditingController controllerName = TextEditingController();
+  TextEditingController controllerAmount = TextEditingController();
+  TextEditingController controllerDescription = TextEditingController();
+  TextEditingController controllerDate = TextEditingController();
+
+  final DatepickerField datePicker =
+      DatepickerField(controller: TextEditingController());
+
   final _formKey = GlobalKey<FormState>();
+
+  Dio dio = Dio();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          backgroundColor: AppColor.backgroundFullScreen,
-          title: Text(
-            "neuer eintrag".toUpperCase(),
-            style: Fonts.textHeadingBold,
-          ),
-          titleTextStyle: TextStyle(color: AppColor.neutral100)),
-      backgroundColor: AppColor.backgroundFullScreen,
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Container(
-          padding: Values.bigCardPadding,
-          decoration: BoxDecoration(
-              border: Border.all(
-                  color: AppColor.neutral500,
-                  width: 0,
-                  style: BorderStyle.solid),
-              borderRadius: BorderRadius.circular(11),
-              color: AppColor.neutral500),
-          margin: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  InputField(
-                    lblText: "Betrag",
-                    formatter: digits,
-                    keyboardType: numeric,
-                  ),
-                  InputField(
-                    lblText: "Unternehmen",
-                    formatter: letters,
-                    keyboardType: text,
-                  ),
-                  InputField(
-                    lblText: "Beschreibung",
-                    formatter: letters,
-                    keyboardType: text,
-                  ),
-                  const DatepickerField(),
-                  const PdfPreview(pdfUrl: '/assets/pdf/sample.pdf'),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // If the form is valid, display a snackbar. In the real world,
-                        // you'd often call a server or save the information in a database.
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Processing Data')),
-                        );
-                      }
-                    },
-                    child: const Text('Submit'),
-                  )
-                ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: AppColor.backgroundGray,
+            title: Text(
+              "neuer eintrag".toUpperCase(),
+              style: Fonts.textHeadingBold,
+            ),
+            titleTextStyle: TextStyle(color: AppColor.neutral100)),
+        backgroundColor: AppColor.backgroundGray,
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            padding: Values.bigCardPadding,
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: AppColor.neutral500,
+                    width: 0,
+                    style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(11),
+                color: AppColor.neutral500),
+            margin: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    InputField(
+                      lblText: "Name",
+                      formatter: letters,
+                      keyboardType: text,
+                      controller: controllerName,
+                    ),
+                    InputField(
+                      lblText: "Betrag",
+                      formatter: digits,
+                      keyboardType: numeric,
+                      controller: controllerAmount,
+                    ),
+                    InputField(
+                      lblText: "Beschreibung",
+                      formatter: letters,
+                      keyboardType: text,
+                      controller: controllerDescription,
+                    ),
+                    DatepickerField(controller: controllerDate),
+                    const PdfPreview(pdfUrl: '/assets/pdf/sample.pdf'),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Daten werden gespeichert')),
+                          );
+
+                          final amount = double.tryParse(controllerAmount.text);
+                          final date = DateTime.parse(controllerDate.text);
+
+                          //final date = datePicker.selectedDate;
+
+                          _sendText(controllerName.text, amount!, date,
+                              controllerDescription.text);
+                        }
+                      },
+                      child: const Text('Submit'),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future _sendText(
+      String name, double amount, DateTime date, String description) async {
+    //final navigator = Navigator.of(context);
+    var formData = FormData.fromMap(
+      {
+        "transaction_name": name,
+        "transaction_amount": amount,
+        "date": date,
+        "description": description,
+        "category_id": "1234",
+        "type_id": "1234",
+        "interval_id": "1234",
+        "account_id": "1234"
+      },
+    );
+    var response = await dio.post("http://10.0.2.2:5432/transactions/test",
+        data: formData);
+    debugPrint(response.toString());
+    // navigator.pop(context);
   }
 }
