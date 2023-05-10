f3bc3341dad3f028ad8ab40dd9fce6965e773922
nofrari <nora.riglthaler@gmail.com>
2023-05-10 13:15:57 +0200

    add dynamic insert of data

--- frontend/lib/widgets/home_overview_card.dart
+++ frontend/lib/widgets/home_overview_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/accounts/accounts_overview.dart';
 import 'package:frontend/widgets/transactions/latest_transactions_list.dart';
+import 'package:dio/dio.dart';
 //import constants
 import '../constants/colors.dart';
 import '../constants/values.dart';
@@ -16,38 +17,99 @@ class HomeOverviewCard extends StatefulWidget {
 }
 
 class _HomeOverviewCardState extends State<HomeOverviewCard> {
-  final List<Transaction> _latestTransactions = [
-    Transaction(
-      id: 1,
-      name: "EBI",
-      amount: 20.00,
-      category: "Shop",
-      date: DateTime(2022, 9, 9),
-      description: "Description",
-      transactionType: 0,
-    ),
-    Transaction(
-      id: 2,
-      name: "EBI 2",
-      amount: 30.30,
-      category: "Shop",
-      date: DateTime(2021, 9, 9),
-      description: "Description",
-      transactionType: 1,
-    ),
-  ];
-  final List<Account> _accounts = [
-    Account(
-      id: 1,
-      name: "EBI",
-      amount: 20,
-    ),
-    Account(
-      id: 2,
-      name: "EBI 2",
-      amount: 30.30,
-    ),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _getTransactions();
+    _getAccounts();
+  }
+
+  final List<Account> accounts = [];
+
+  void _getTransactions() async {
+    try {
+      Response response =
+          await Dio().get('http://localhost:5432/transactions/list/1234');
+      List<Transaction> transactions = [];
+
+      for (var i = 0; i < 2 && i < response.data.length; i++) {
+        transactions.add(
+          Transaction(
+              id: response.data[i]['transaction_id'].toString(),
+              name: response.data[i]['transaction_name'].toString(),
+              amount: double.parse(
+                  response.data[i]['transaction_amount'].toString()),
+              category: response.data[i]['category_id'].toString(),
+              date: DateTime.parse(response.data[i]['date']),
+              transactionType: int.parse(response.data[i]['type_id']),
+              description: response.data[i]['description'].toString()),
+        );
+      }
+      setState(() {
+        _latestTransactions = transactions;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _getAccounts() async {
+    try {
+      Response response =
+          await Dio().get('http://localhost:5432/accounts/list/1234');
+
+      for (var i = 0; i < response.data.length; i++) {
+        accounts.add(Account(
+          id: response.data[i]['account_id'].toString(),
+          name: response.data[i]['account_name'].toString(),
+          amount: double.parse(response.data[i]['account_balance'].toString()),
+        ));
+      }
+
+      setState(() {
+        _allAccounts = accounts;
+        print(_allAccounts);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  List<Transaction> _latestTransactions = [];
+  List<Account> _allAccounts = [];
+
+  //  List<Transaction> _latestTransactions = [
+  //   Transaction(
+  //     id: 1,
+  //     name: "EBI",
+  //     amount: 20.00,
+  //     category: "Shop",
+  //     date: DateTime(2022, 9, 9),
+  //     description: "Description",
+  //     transactionType: 0,
+  //   ),
+  //   Transaction(
+  //     id: 2,
+  //     name: "EBI 2",
+  //     amount: 30.30,
+  //     category: "Shop",
+  //     date: DateTime(2021, 9, 9),
+  //     description: "Description",
+  //     transactionType: 1,
+  //   ),
+  // ];
+  // final List<Account> _accounts = [
+  //   Account(
+  //     id: 1,
+  //     name: "EBI",
+  //     amount: 20,
+  //   ),
+  //   Account(
+  //     id: 2,
+  //     name: "EBI 2",
+  //     amount: 30.30,
+  //   ),
+  // ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -60,7 +122,7 @@ class _HomeOverviewCardState extends State<HomeOverviewCard> {
         children: [
           LatestTransactionList(transactions: _latestTransactions),
           AccountsOverview(
-            accounts: _accounts,
+            accounts: accounts,
           )
         ],
       ),
