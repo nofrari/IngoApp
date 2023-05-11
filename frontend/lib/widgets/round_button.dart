d06a5f931615c90702085b96bbd627e9a3dbc8e2
itsjustmoniii <suessmonika8@gmail.com>
2023-05-11 10:22:40 +0200

    edit round-button

--- frontend/lib/widgets/round_button.dart
+++ frontend/lib/widgets/round_button.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:frontend/constants/colors.dart';
 
 class RoundButton extends StatelessWidget {
-  const RoundButton({required this.onTap, required this.icon, super.key});
+  RoundButton(
+      {this.isTransparent, required this.onTap, required this.icon, super.key});
 
   final IconData icon;
   final void Function() onTap;
+  bool? isTransparent;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColor.neutral800,
+        backgroundColor: isTransparent != null && isTransparent == true
+            ? Colors.transparent
+            : AppColor.neutral800,
         padding: const EdgeInsets.all(8),
         shape: const CircleBorder(),
         side: BorderSide(width: 2, color: AppColor.blueActive),
