000798c2664e8211a1555872417ff46c4d4545cf
itsjustmoniii <suessmonika8@gmail.com>
2023-05-15 15:30:54 +0200

    update categories page

--- frontend/lib/pages/categories/categories.dart
+++ frontend/lib/pages/categories/categories.dart
@@ -1,10 +1,10 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:frontend/constants/colors.dart';
 import 'package:frontend/constants/fonts.dart';
 import 'package:frontend/pages/categories/category_add.dart';
 import 'package:frontend/pages/categories/category_edit.dart';
 import 'package:frontend/pages/profile.dart';
+import 'package:frontend/services/initial_service.dart';
 import 'package:frontend/widgets/button.dart';
 import 'package:frontend/widgets/button_transparent_container.dart';
 import 'package:frontend/widgets/categories/category_item.dart';
@@ -12,6 +12,10 @@ import 'package:frontend/widgets/header.dart';
 import 'package:dio/dio.dart';
 import 'package:frontend/widgets/text_google.dart';
 import 'package:frontend/models/category.dart';
+import 'package:frontend/models/color.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/icon.dart';
 
 class Categories extends StatefulWidget {
   const Categories({super.key});
@@ -22,76 +26,53 @@ class Categories extends StatefulWidget {
 
 class _CategoriesState extends State<Categories> {
   Dio dio = Dio();
-
-  List<Map<String, dynamic>> categoryItemsData = [
-    {
-      'bgColor': 'pink',
-      'isBlack': true,
-      'icon': 'cookie',
-      'label': "Auto",
-    },
-    {
-      'bgColor': 'pink',
-      'isBlack': false,
-      'icon': 'cookie',
-      'label': "Bildung",
-    },
-    {
-      'bgColor': 'pink',
-      'isBlack': true,
-      'icon': 'delete',
-      'label': "Essen",
-    },
-    {
-      'bgColor': 'pink',
-      'isBlack': false,
-      'icon': 'add',
-      'label': "Studium",
-    },
-    {
-      'bgColor': 'pink',
-      'isBlack': false,
-      'icon': 'delete',
-      'label': "Running",
-    },
-    {
-      'bgColor': 'pink',
-      'isBlack': true,
-      'icon': 'add',
-      'label': "Auto",
-    },
-  ];
-
-  List<Category> categories = [
-    Category(
-        category_id: "1",
-        bgColor: 'pink',
-        isBlack: true,
-        icon: 'add',
-        label: 'Essen'),
-    Category(
-        category_id: "2",
-        bgColor: 'orange',
-        isBlack: false,
-        icon: 'delete',
-        label: 'Uni')
-  ];
+  List<CategoryModel> categories = [];
+  List<IconModel> icons = [];
+  List<ColorModel> colors = [];
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getData();
-    //liste categorien von der datenbank fetchen und in preferences schmeißen
   }
 
-  void getData() async {
-    var response = await dio.get("http://localhost:5432/categories/data");
-    //response listen zu categorie liste und in shared pref
+  List<CategoryModel> categoryList = [];
+
+  Future<List<CategoryModel>> getData() async {
+    try {
+      var response = await dio.get("http://localhost:5432/categories/1");
+      List<CategoryModel> categoryList = []; // move the declaration here
+      if (response.data['categories'] != null) {
+        for (var category in response.data['categories']) {
+          IconModel desiredIcon =
+              icons.firstWhere((icon) => icon.icon_id == category['icon_id']);
+          ColorModel desiredColor = colors
+              .firstWhere((color) => color.color_id == category['color_id']);
+          categoryList.add(
+            CategoryModel(
+              category_id: category['category_id'],
+              bgColor: desiredColor.name,
+              isBlack: category['is_black'],
+              icon: desiredIcon.name,
+              label: category['category_name'],
+            ),
+          );
+        }
+        context.read<InitialService>().setCategories(categoryList);
+      }
+
+      return categoryList;
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return [];
   }
 
   @override
   Widget build(BuildContext context) {
+    // context.read<InitialService>().setCategories(categoryList);
+    colors = context.watch<InitialService>().getColors();
+    icons = context.watch<InitialService>().getIcons();
+
     return Scaffold(
       appBar: Header(
         onTap: () {
@@ -109,74 +90,94 @@ class _CategoriesState extends State<Categories> {
         ),
       ),
       backgroundColor: AppColor.backgroundFullScreen,
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: SingleChildScrollView(
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: AppColor.neutral500,
-                            width: 0,
-                            style: BorderStyle.solid),
-                        borderRadius: BorderRadius.circular(30),
-                        color: AppColor.neutral500,
-                      ),
+      body: FutureBuilder<List<CategoryModel>>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<CategoryModel> categories = snapshot.data!;
+            return Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 20),
                       child: Column(
-                        children: <Widget>[
-                          ...categories
-                              .map((categoryItemsData) => CategoryItem(
-                                    onTap: () async {
-                                      await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => CategoryEdit(
-                                            category: categoryItemsData,
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    bgColor: categoryItemsData.bgColor,
-                                    isBlack: categoryItemsData.isBlack,
-                                    icon: categoryItemsData.icon,
-                                    label: categoryItemsData.label,
-                                    isSmall: true,
-                                  ))
-                              .toList(),
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: AppColor.neutral500,
+                                  width: 0,
+                                  style: BorderStyle.solid),
+                              borderRadius: BorderRadius.circular(30),
+                              color: AppColor.neutral500,
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                ...categories
+                                    .map(
+                                      (categoryItemsData) => CategoryItem(
+                                        onTap: () async {
+                                          await Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  CategoryEdit(
+                                                category: categoryItemsData,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        bgColor: categoryItemsData.bgColor,
+                                        isBlack: categoryItemsData.isBlack,
+                                        icon: categoryItemsData.icon,
+                                        label: categoryItemsData.label,
+                                        isSmall: true,
+                                      ),
+                                    )
+                                    .toList(),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 100),
                         ],
                       ),
                     ),
-                    const SizedBox(height: 100),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-          ButtonTransparentContainer(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              child: Button(
-                  isTransparent: true,
-                  btnText: "KATEGORIE HINZUFÜGEN",
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const CategoryAdd(),
-                      ),
-                    );
-                  },
-                  theme: ButtonColorTheme.secondary),
-            ),
-          ),
-        ],
+                ButtonTransparentContainer(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                    child: Button(
+                        isTransparent: true,
+                        btnText: "KATEGORIE HINZUFÜGEN",
+                        onTap: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CategoryAdd(),
+                            ),
+                          );
+                        },
+                        theme: ButtonColorTheme.secondary),
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
