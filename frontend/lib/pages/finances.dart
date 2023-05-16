0fa78df802e1107aa46e22a1a3b61b5d8231799f
itsjustmoniii <suessmonika8@gmail.com>
2023-05-16 16:13:50 +0200

    update finance-page

--- frontend/lib/pages/finances.dart
+++ frontend/lib/pages/finances.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/color.dart';
+import 'package:frontend/models/icon.dart';
+
+import 'package:frontend/widgets/transactions/transaction_list.dart';
+import 'package:provider/provider.dart';
+
+import '../models/transaction.dart';
+import '../services/initial_service.dart';
+import '../services/transaction_service.dart';
 
 class Finances extends StatefulWidget {
   const Finances({super.key});
@@ -8,11 +17,19 @@ class Finances extends StatefulWidget {
 }
 
 class _FinancesState extends State<Finances> {
+  List<TransactionModel> transactions = [];
+  List<ColorModel> colors = [];
+  List<IconModel> icons = [];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Finances Screen'),
+    transactions = context.watch<TransactionService>().getTransactions();
+    colors = context.watch<InitialService>().getColors();
+    icons = context.watch<InitialService>().getIcons();
+    debugPrint("${transactions.toList().toString()}");
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: TransactionList(
+        transactions: transactions,
       ),
     );
   }
