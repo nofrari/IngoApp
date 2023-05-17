37d2d3d6cfd730184cd67285b77e7506ee9cdad4
itsjustmoniii <suessmonika8@gmail.com>
2023-05-17 15:35:58 +0200

    edit transaction-list

--- frontend/lib/widgets/transactions/transaction_list.dart
+++ frontend/lib/widgets/transactions/transaction_list.dart
@@ -2,14 +2,20 @@ import 'package:frontend/constants/colors.dart';
 import 'package:frontend/models/category.dart';
 import 'package:frontend/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/services/accounts_service.dart';
 import 'package:frontend/widgets/transactions/transaction_item.dart';
 import 'package:provider/provider.dart';
+import '../../models/account.dart';
 import '../../services/initial_service.dart';
 
 class TransactionList extends StatefulWidget {
   const TransactionList(
-      {super.key, this.selectedCategory, required this.transactions});
+      {super.key,
+      this.selectedCategory,
+      this.accounts,
+      required this.transactions});
   final String? selectedCategory;
+  final String? accounts;
   final List<TransactionModel> transactions;
 
   @override
@@ -18,20 +24,46 @@ class TransactionList extends StatefulWidget {
 
 class _TransactionListState extends State<TransactionList> {
   List<CategoryModel> categories = [];
+  List<Account> accounts = [];
 
   @override
   Widget build(BuildContext context) {
     categories = context.watch<InitialService>().getCategories();
+    accounts = context.watch<AccountsService>().getAccounts();
 
     List<TransactionModel> filteredTransactions = widget.transactions;
 
-    if (widget.selectedCategory != null) {
-      CategoryModel desiredCategory = categories
-          .firstWhere((category) => category.label == widget.selectedCategory);
-      filteredTransactions = filteredTransactions
-          .where((transaction) =>
-              transaction.category_id == desiredCategory.category_id)
+    if ((widget.selectedCategory != null && widget.selectedCategory != "") ||
+        (widget.accounts != null && widget.accounts != "")) {
+      List<String> selectedCategories = widget.selectedCategory!
+          .split(',')
+          .map((category) => category.trim())
           .toList();
+      List<String> selectedAccounts =
+          widget.accounts!.split(',').map((account) => account.trim()).toList();
+      List<CategoryModel> desiredCategories = categories
+          .where((category) => selectedCategories.contains(category.label))
+          .toList();
+      List<Account> desiredAccounts = accounts
+          .where((account) => selectedAccounts.contains(account.name))
+          .toList();
+
+      filteredTransactions = filteredTransactions.where((transaction) {
+        bool categoryFilter = true;
+        bool accountFilter = true;
+
+        if (desiredCategories.isNotEmpty) {
+          categoryFilter = desiredCategories.any(
+              (category) => transaction.category_id == category.category_id);
+        }
+
+        if (desiredAccounts.isNotEmpty) {
+          accountFilter = desiredAccounts
+              .any((account) => transaction.account_id == account.id);
+        }
+
+        return categoryFilter && accountFilter;
+      }).toList();
     }
     //for long lists use ListView
     return Container(
