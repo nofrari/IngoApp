7b1e0a4b9be557e958fe6f75f487ffbca27f990d
itsjustmoniii <suessmonika8@gmail.com>
2023-05-17 13:58:37 +0200

    update transaction-list

--- frontend/lib/widgets/transactions/transaction_list.dart
+++ frontend/lib/widgets/transactions/transaction_list.dart
@@ -1,16 +1,15 @@
 import 'package:frontend/constants/colors.dart';
-import 'package:frontend/constants/fonts.dart';
+import 'package:frontend/models/category.dart';
 import 'package:frontend/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/categories/categories.dart';
-import 'package:frontend/widgets/button.dart';
 import 'package:frontend/widgets/transactions/transaction_item.dart';
-
-import '../../constants/values.dart';
+import 'package:provider/provider.dart';
+import '../../services/initial_service.dart';
 
 class TransactionList extends StatefulWidget {
-  const TransactionList({super.key, required this.transactions});
-
+  const TransactionList(
+      {super.key, this.selectedCategory, required this.transactions});
+  final String? selectedCategory;
   final List<TransactionModel> transactions;
 
   @override
@@ -18,8 +17,22 @@ class TransactionList extends StatefulWidget {
 }
 
 class _TransactionListState extends State<TransactionList> {
+  List<CategoryModel> categories = [];
+
   @override
   Widget build(BuildContext context) {
+    categories = context.watch<InitialService>().getCategories();
+
+    List<TransactionModel> filteredTransactions = widget.transactions;
+
+    if (widget.selectedCategory != null) {
+      CategoryModel desiredCategory = categories
+          .firstWhere((category) => category.label == widget.selectedCategory);
+      filteredTransactions = filteredTransactions
+          .where((transaction) =>
+              transaction.category_id == desiredCategory.category_id)
+          .toList();
+    }
     //for long lists use ListView
     return Container(
       margin: const EdgeInsets.only(top: 20),
@@ -34,9 +47,9 @@ class _TransactionListState extends State<TransactionList> {
       ),
       child: ListView.builder(
         shrinkWrap: true,
-        itemCount: widget.transactions.length,
+        itemCount: filteredTransactions.length,
         itemBuilder: (context, index) {
-          final transaction = widget.transactions[index];
+          final transaction = filteredTransactions[index];
           return TransactionItem(transaction: transaction);
         },
       ),
