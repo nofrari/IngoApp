445267e91a4a6b872cc37568a0e6280a79bf3aac
itsjustmoniii <suessmonika8@gmail.com>
2023-05-17 15:36:32 +0200

    edit finances page

--- frontend/lib/pages/finances.dart
+++ frontend/lib/pages/finances.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:frontend/models/category.dart';
 import 'package:frontend/models/color.dart';
 import 'package:frontend/models/icon.dart';
+import 'package:frontend/services/accounts_service.dart';
 import 'package:frontend/widgets/input_fields/multiselect_dropdown.dart';
 
 import 'package:frontend/widgets/transactions/transaction_list.dart';
 import 'package:provider/provider.dart';
 
 import '../constants/strings.dart';
+import '../models/account.dart';
 import '../models/transaction.dart';
 import '../services/initial_service.dart';
 import '../services/transaction_service.dart';
@@ -26,38 +28,29 @@ class _FinancesState extends State<Finances> {
   List<ColorModel> colors = [];
   List<IconModel> icons = [];
   List<CategoryModel> categories = [];
-  List<String> selectedCategories = [];
+  List<Account> accounts = [];
   String? selectedCategory;
+  String? selectedAccount;
   String? selectedStartDate;
-
-  void onCategorySelected(String category) {
-    setState(() {
-      selectedCategory = category;
-    });
-    debugPrint("${selectedCategory}");
-  }
 
   void onStartDateSelected(String startDate) {
     setState(() {
       selectedStartDate = startDate;
     });
   }
-  // void onCategorySelected(String category) {
-  //   setState(() {
-  //     if (selectedCategories.contains(category)) {
-  //       selectedCategories.remove(category);
-  //     } else {
-  //       selectedCategories.add(category);
-  //     }
-  //   });
-  //   debugPrint(selectedCategories.toString());
-  // }
 
-  String _selectedType = "";
-  void setSelectedType(String type) {
+  void onCategorySelected(List<String> values) {
     setState(() {
-      _selectedType = type;
+      selectedCategory = values.join(", ");
     });
+    debugPrint(selectedCategory);
+  }
+
+  void onAccountSelected(List<String> values) {
+    setState(() {
+      selectedAccount = values.join(", ");
+    });
+    debugPrint(selectedAccount);
   }
 
   TextEditingController controllerStartDate = TextEditingController();
@@ -69,6 +62,7 @@ class _FinancesState extends State<Finances> {
     categories = context.watch<InitialService>().getCategories();
     colors = context.watch<InitialService>().getColors();
     icons = context.watch<InitialService>().getIcons();
+    accounts = context.watch<AccountsService>().getAccounts();
 
     return Container(
       margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
@@ -83,25 +77,21 @@ class _FinancesState extends State<Finances> {
               label: "End",
               controller: controllerEndDate,
             ),
-            Dropdown(
-              label: Strings.dropdownFinancesCategories,
-              setValue: (value) {
-                onCategorySelected(value);
-              },
-              dropdownItems:
-                  categories.map((category) => category.label).toList(),
-              needsNewCategoryButton: false,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Das Feld darf nicht leer sein';
-                }
-                return null;
-              },
-            ),
+
             MultiSelectDropdownField(
               dropdownItems:
                   categories.map((category) => category.label).toList(),
-              label: "Kategorien",
+              setValues: (value) {
+                onCategorySelected(value);
+              },
+              label: "Kategorie",
+            ),
+            MultiSelectDropdownField(
+              dropdownItems: accounts.map((account) => account.name).toList(),
+              setValues: (value) {
+                onAccountSelected(value);
+              },
+              label: "Konto",
             ),
 
             // MultipleDropdown(
@@ -131,9 +121,9 @@ class _FinancesState extends State<Finances> {
             //   },
             // ),
             TransactionList(
-              selectedCategory: selectedCategory,
-              transactions: transactions,
-            ),
+                selectedCategory: selectedCategory,
+                transactions: transactions,
+                accounts: selectedAccount),
           ],
         ),
       ),
