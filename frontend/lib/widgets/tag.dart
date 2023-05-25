3e89178cab658d6c9ff78a526b80dceac6d9fd0f
itsjustmoniii <suessmonika8@gmail.com>
2023-05-25 11:24:17 +0200

    update tag

--- frontend/lib/widgets/tag.dart
+++ frontend/lib/widgets/tag.dart
@@ -5,25 +5,18 @@ import '../constants/fonts.dart';
 import '../constants/values.dart';
 
 class Tag extends StatelessWidget {
-  const Tag(
-      {required this.btnText,
-      required this.onTap,
-      this.isTransparent,
-      super.key});
+  const Tag({required this.btnText, this.onTap, this.isCategory, super.key});
 
   final String btnText;
-  final void Function() onTap;
-  final bool? isTransparent;
+  final void Function()? onTap;
+  final bool? isCategory;
 
   @override
   Widget build(BuildContext context) {
-    TextStyle buttonText;
     Color backgroundColor;
-    Color borderColor;
-
-    buttonText = Fonts.primaryButton;
-    backgroundColor = AppColor.blueActive;
-    borderColor = AppColor.blueActive;
+    backgroundColor = isCategory != null && isCategory == true
+        ? AppColor.violett
+        : AppColor.blueActive;
 
     return IntrinsicWidth(
       child: Container(
@@ -34,19 +27,19 @@ class Tag extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 10),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15),
-              side: BorderSide(color: borderColor, width: 2),
             ),
           ),
           child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 5),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
                   btnText,
                   style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: AppColor.neutral600),
+                    fontWeight: FontWeight.normal,
+                    color: AppColor.neutral600,
+                  ),
                 ),
                 Icon(
                   Icons.close,
