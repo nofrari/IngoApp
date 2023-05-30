8c14b057c3d1dd02bb32605150e34fab934f2e3b
itsjustmoniii <suessmonika8@gmail.com>
2023-05-30 14:19:30 +0200

    update strings

--- frontend/lib/constants/strings.dart
+++ frontend/lib/constants/strings.dart
@@ -2,7 +2,7 @@ class Strings {
   // Strings._() = private constructor --> prevents Class from being instantiated
   Strings._();
   static const String menuHome = "Home";
-  static const String menuBudget = "Budget";
+  static const String menuCategories = "Kategorien";
   static const String menuFinance = "Finanzen";
   static const String menuAccounts = "Accounts";
 
