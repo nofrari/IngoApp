fb9151ed6505463d3290cb8a763da78d76cfe61e
itsjustmoniii <suessmonika8@gmail.com>
2023-06-06 11:32:25 +0200

    remove height on container

--- frontend/lib/pages/profile/delete_profile.dart
+++ frontend/lib/pages/profile/delete_profile.dart
@@ -38,100 +38,98 @@ class _DeleteProfileState extends State<DeleteProfile> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async => false,
-        child: Scaffold(
-            appBar: Header(
-              element: Text(
-                Strings.profileDelete,
-                style: Fonts.textHeadingBold,
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: Header(
+          element: Text(
+            Strings.profileDelete,
+            style: Fonts.textHeadingBold,
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: AppColor.backgroundFullScreen,
+        body: Column(mainAxisSize: MainAxisSize.min, children: [
+          Container(
+            width: 130,
+            height: 130,
+            padding: const EdgeInsets.fromLTRB(28, 32, 28, 28),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: AppColor.blueActive,
+              border: null,
             ),
-            backgroundColor: AppColor.backgroundFullScreen,
-            body: SingleChildScrollView(
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Container(
-                  width: 130,
-                  height: 130,
-                  padding: const EdgeInsets.fromLTRB(28, 32, 28, 28),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColor.blueActive,
-                    border: null,
+            child: Center(
+              child: Text(
+                user.abreviationName,
+                style: Fonts.userIconBig,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: Values.bigCardMargin,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Möchtest du dein Konto wirklich löschen?".toUpperCase(),
+                    style: Fonts.text300,
+                    textAlign: TextAlign.center,
                   ),
-                  child: Center(
+                  Padding(
+                    padding: Values.bigCardPadding,
                     child: Text(
-                      user.abreviationName,
-                      style: Fonts.userIconBig,
+                      "Dein Profil, deine Einstellungen und alle Daten der App werden dauerhaft gelöscht.",
+                      style: Fonts.text150,
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
-                Container(
-                  margin: Values.bigCardMargin,
-                  height: MediaQuery.of(context).size.height * 0.63,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Möchtest du dein Konto wirklich löschen?"
-                            .toUpperCase(),
-                        style: Fonts.text300,
-                        textAlign: TextAlign.center,
+                  const Spacer(),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: Values.buttonPadding,
+                        child: Button(
+                            btnText: Strings.profileDelete,
+                            onTap: () async {
+                              try {
+                                var response = await dio.get(
+                                    "${Values.serverURL}/users/delete/${user.id}");
+                                await context.read<ProfileService>().setUser(
+                                    id: "",
+                                    firstname: "",
+                                    lastname: "",
+                                    email: "");
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Auth(),
+                                  ),
+                                );
+                                print("funktioniert");
+                              } catch (e) {
+                                debugPrint(e.toString());
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      behavior: SnackBarBehavior.floating,
+                                      content:
+                                          Text('Ein Fehler ist aufgetreten.')),
+                                );
+                              }
+                            },
+                            theme: ButtonColorTheme.secondaryDark),
                       ),
-                      Padding(
-                        padding: Values.bigCardPadding,
-                        child: Text(
-                          "Dein Profil, deine Einstellungen und alle Daten der App werden dauerhaft gelöscht.",
-                          style: Fonts.text150,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const Spacer(),
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Padding(
-                            padding: Values.buttonPadding,
-                            child: Button(
-                                btnText: Strings.profileDelete,
-                                onTap: () async {
-                                  try {
-                                    var response = await dio.get(
-                                        "${Values.serverURL}/users/delete/${user.id}");
-                                    await context
-                                        .read<ProfileService>()
-                                        .setUser(
-                                            id: "",
-                                            firstname: "",
-                                            lastname: "",
-                                            email: "");
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Auth(),
-                                      ),
-                                    );
-                                    print("funktioniert");
-                                  } catch (e) {
-                                    debugPrint(e.toString());
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                          behavior: SnackBarBehavior.floating,
-                                          content: Text(
-                                              'Ein Fehler ist aufgetreten.')),
-                                    );
-                                  }
-                                },
-                                theme: ButtonColorTheme.secondaryDark),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ]),
-            )));
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
