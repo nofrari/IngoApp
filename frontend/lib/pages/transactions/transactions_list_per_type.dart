ecf52ea3039cce379b951d54ac69bb6210216e41
wooki34512 <sebastian.koller7@gmx.at>
2023-06-12 12:05:44 +0200

    fix recurring transactions list

--- frontend/lib/pages/transactions/transactions_list_per_type.dart
+++ frontend/lib/pages/transactions/transactions_list_per_type.dart
@@ -14,8 +14,24 @@ class TransactionsTypeListItem extends StatelessWidget {
   List<TransactionModel> transactions;
   IntervalModel interval;
 
+  int howOftenExists(
+      IntervalModel interval, List<TransactionModel> transactions) {
+    int count = 0;
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].interval_id == interval.interval_id) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  Set<String> displayedIds = {};
+  // Set zum Verfolgen der bereits angezeigten IDs
   @override
   Widget build(BuildContext context) {
+    if (howOftenExists(interval, transactions) == 0) {
+      return Container();
+    }
     return Container(
       margin: const EdgeInsets.only(bottom: 20),
       child: Column(
@@ -31,22 +47,31 @@ class TransactionsTypeListItem extends StatelessWidget {
             shrinkWrap: true,
             itemCount: transactions.length,
             itemBuilder: (context, index) {
-              return (transactions[index].interval_id == interval.interval_id)
-                  ? Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 0, vertical: 5),
-                      decoration: BoxDecoration(
-                          border: const Border.fromBorderSide(BorderSide.none),
-                          borderRadius: BorderRadius.circular(8),
-                          color: AppColor.neutral700),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 15, vertical: 10),
-                        child: TransactionItem(
-                            transaction: transactions[index], isSmall: true),
-                      ),
-                    )
-                  : Container();
+              if (transactions[index].interval_id == interval.interval_id) {
+                if (displayedIds.contains(transactions[index].transaction_id)) {
+                  // Wenn die ID bereits angezeigt wurde, überspringe die aktuelle Transaktion
+                  return Container();
+                }
+
+                // Die ID wurde noch nicht angezeigt, füge sie zum Set hinzu und zeige die Transaktion an
+                displayedIds.add(transactions[index].transaction_id);
+
+                return Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 5),
+                  decoration: BoxDecoration(
+                      border: const Border.fromBorderSide(BorderSide.none),
+                      borderRadius: BorderRadius.circular(8),
+                      color: AppColor.neutral700),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    child: TransactionItem(
+                        transaction: transactions[index], isSmall: true),
+                  ),
+                );
+              } else
+                return Container();
             },
           )
         ],
